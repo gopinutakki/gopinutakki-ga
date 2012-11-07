9551e16b3ac7a057bf916f1c7d3e73da80b8ef7b
jcwajna <jcwajna@gmail.com>
2012-11-07 22:27:51 +0100

    init: projekt startowy, odpalenie przykladow Watchmaker

---
+++ src/main/java/pl/edu/agh/niching/App.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.niching;
+
+import org.uncommons.watchmaker.examples.Launcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Launcher.main(new String[]{"bits"});
+    }
+}
